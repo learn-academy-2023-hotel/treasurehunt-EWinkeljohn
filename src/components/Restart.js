f307b2ecccb8b9e39a01c528a017d1bc679b87d2
EWinkeljohn <emcwink@gmail.com>
2023-10-30 14:24:56 -0700

    all emojis and restart button working

---
+++ src/components/Restart.js
@@ -0,0 +1,17 @@
+// import React from "react"
+
+// const Restart = ({value, index, restartClick}) => {
+
+//     const refresh = () => {
+//         restartClick(value)
+//     }
+//     return (
+//         <>
+//         <button className="start-button" onClick={refresh}>
+//         Restart!
+//         </button>
+//         </>
+//         )
+//     }
+
+//  export default Restart
